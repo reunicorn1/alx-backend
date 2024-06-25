3077dd29e52a97a841626e1efdd1c52b94545429
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-25 13:59:37 +0400

    3-hypermedia_del_pagination.py - getting around the default value

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -37,20 +37,18 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
+    def get_hyper_index(self, index: int = 0,
                         page_size: int = 10) -> Dict:
         """
         This function returns metadata about the paginated dataset
         to help clients request for the next page and also giving them
         information about their current request
-
         Parameters
         ----------
         index: Optional[int]
             the start index of your query
         page_size: int
             the size of the requested dataset
-
         Returns
         -------
         A dictionary with the following key-value pairs:
@@ -63,25 +61,20 @@ class Server:
         data: List[List[str]]
             the actual page of the dataset
         """
-        data = self.indexed_dataset()
-        assert index is not None and index >= 0 and index <= max(
-                data.keys())
-        page_data = []
-        data_count = 0
-        next_index = None
-        start = index if index else 0
-        for i, item in data.items():
-            if i >= start and data_count < page_size:
-                page_data.append(item)
-                data_count += 1
-                continue
-            if data_count == page_size:
-                next_index = i
-                break
-        page_info = {
-            'index': index,
-            'next_index': next_index,
-            'page_size': len(page_data),
-            'data': page_data,
-        }
-        return page_info
+        assert isinstance(page_size, int)
+        if index is None:
+            index = 0
+        assert isinstance(index, int) and index >= 0 and index < len(
+                self.__dataset)
+        data = []
+        next_index = index
+        for item in range(page_size):
+            while not self.__indexed_dataset.get(next_index):
+                next_index += 1
+            data.append(self.__indexed_dataset.get(next_index))
+            next_index += 1
+        return {
+                'index': index,
+                'next_index': next_index,
+                'data': data
+                }
