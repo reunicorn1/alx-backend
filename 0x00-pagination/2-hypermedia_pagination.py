c06fa227d14c306a107110a3f85c55390cdb5b31
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-25 13:37:24 +0400

    2-hypermedia_pagination.py fixing type hint and replace | with Union

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@
 """
 import csv
 import math
-from typing import List, Tuple, Dict, Any
+from typing import List, Tuple, Dict, Any, Union
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -62,12 +62,12 @@ class Server:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
-        if len(self.__dataset) < start:
+        if len(self.dataset()) < start:
             return []
         return self.dataset()[start: end]
 
     def get_hyper(self, page: int = 1, page_size: int
-                  = 10) -> Dict[str, int | List[List[int]] | None]:
+                  = 10) -> Dict[str, Union[int, List[List[Any]], None]]:
         """
         This function provide metadata about the request while
         retrieving data from the dataset
@@ -97,15 +97,15 @@ class Server:
                 "page": page,
                 "data": [],
                 "next_page": None if page * page_size >= len(
-                    self.dataset) else page + 1,
+                    self.dataset()) else page + 1,
                 "prev_page": page - 1 if page > 1 else None,
-                "total_pages": (len(self.dataset) + page_size - 1) // page_size
+                "total_pages": (len(self.dataset()) + page_size - 1) // page_size
             }
 
         page_data = self.get_page(page, page_size)
         start, end = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
-        page_info = {
+        page_info: Dict[str, Union[int, List[List[Any]], None]] = {
             'page_size': len(page_data),
             'page': page,
             'data': page_data,
