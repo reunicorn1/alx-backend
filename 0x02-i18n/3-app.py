47f6a927491f81bd24a7648352c68ab8b84e396f
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-03 03:41:59 +0400

    pycodestyle in 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,6 +8,7 @@ Starting a web application listenting on
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config:
     """
     This class stores the configuraiton of the web application
@@ -26,17 +27,16 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
-#babel = Babel(app)
+babel = Babel(app)
 
 
-#@babel.localeselector
+@babel.localeselector
 def get_locale() -> str:
     """
     This function determine the lanaguage to be used
     """
     return request.accept_languages.best_match(
             app.config['LANGUAGES'])
-babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
