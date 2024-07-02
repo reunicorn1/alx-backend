f8c20c399c55b2c8d1ba7ee8ba4c5bdf67ff66b5
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-02 22:58:37 +0400

    3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+2. Get locale from request
+
+Starting a web application listenting on
+0.0.0.0, port 5000
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config:
+    """
+    This class stores the configuraiton of the web application
+    server
+
+    Properties:
+    ----------
+    LANGUAGES: The list of supported languages
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    This function determine the lanaguage to be used
+    """
+    return request.accept_languages.best_match(
+            app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    This is the entry point to the application
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
