4210c480cbfabff17cace6125824103ed412a59f
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-31 00:08:42 +0400

    create the job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -6,7 +6,7 @@ const jobData = {
     message: "Hello Unicorn!",
   }
 
-const push_notification_code = queue.create('message', jobData).save()
+const push_notification_code = queue.create('push_notification_code', jobData).save()
     .on('enqueue', () => console.log('Notification job created:', push_notification_code.id))
     .on('complete', () => console.log('Notification job completed'))
     .on('failed', () => console.log('Notification job failed'))
