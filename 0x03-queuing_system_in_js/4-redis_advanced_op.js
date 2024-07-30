ae087f34ef88425daf210173d227b82c082da4e1
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-30 18:11:32 +0400

    node redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+const redis = require('redis');
+
+const client = createClient({
+    socket: {
+      host: '127.0.0.1',
+      port: 6379,
+    }
+})
+  .on('error', (err) => console.log('Redis client not connected to the server: ', err))
+  .on('connect', () => console.log('Redis client connected to the server'));
+    
+client.connect();
+
+async function hashSet (field, key, value) {
+    const hsetResult = await client.hSet(field , key, value)
+    console.log('Reply:', hsetResult);
+}
+
+async function getSet (field) {
+    const hGetAllResult = await client.hGetAll(field);
+    console.log(hGetAllResult);
+}
+
+const field = 'HolbertonSchools'
+const dictionary = {
+    Portland: 50,
+    Seattle: 80,
+    'New York': 20,
+    Bogota: 20,
+    Cali: 40,
+    Paris: 2
+}
+for (const [key, value] of Object.entries(dictionary)) {
+    hashSet(field, key, value);
+  }
+
+getSet(field);
