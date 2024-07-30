50144a9205275b502eda8cef44283fdbeeb8e47d
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-30 17:29:28 +0400

    node redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,29 @@
+import { createClient } from 'redis';
+const redis = require('redis');
+
+const client = createClient({
+    socket: {
+      host: '127.0.0.1',
+      port: 6379,
+    }
+})
+  .on('error', (err) => console.log('Redis client not connected to the server: ', err))
+  .on('connect', () => console.log('Redis client connected to the server'));
+    
+client.connect();
+
+async function setNewSchool (schoolName, value) {
+    const reply = await client.set(schoolName, value); // usage of redis.print doen't seem to work
+    console.log('Reply:', reply);
+}
+
+
+async function displaySchoolValue(schoolName) { // redis replaced the callback feature with async/await
+    const value = await client.get(schoolName);
+    console.log(value);
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
